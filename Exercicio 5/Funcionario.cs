aab1a1f33c7e608f6114a34bc93292eab7f6d9d0
Quiel <Quiel@DESKTOP-QPM4FGN>
2021-12-05 01:48:01 -0300

    Add project files.

---
+++ Exercicio 5/Funcionario.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercicio_5
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public double SalarioBruto { get; set; }
+        public double Imposto { get; set; }
+
+        public Funcionario(string nome, double salarioBruto, double imposto)
+        {
+            Nome = nome;
+            SalarioBruto = salarioBruto;
+            Imposto = imposto;
+        }
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += (SalarioBruto * porcentagem) / 100;     
+        }
+
+        public override string ToString()
+        {
+            return Nome +
+                ", $" +
+                SalarioLiquido().ToString("F2");
+        }
+    }
+}
---
+++ Exercicio 5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercicio_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Nome:");
+                string nome = Console.ReadLine();
+
+                Console.Write("Salário bruto:");
+                double salarioBruto = double.Parse(Console.ReadLine());
+
+                Console.Write("Imposto:");
+                double imposto = double.Parse(Console.ReadLine());
+
+                Funcionario funcionario = new Funcionario(nome, salarioBruto, imposto);
+
+                Console.WriteLine($"\nFuncionário {funcionario}");
+
+                Console.Write("\nDigite a porcentagem para aumentar o salário:");
+                double porcentagem = double.Parse(Console.ReadLine());
+                funcionario.AumentarSalario(porcentagem);
+
+                Console.WriteLine($"\nDados atualizados {funcionario}");
+
+                Console.WriteLine("\nPressione qualquer tecla para finalizar");
+                Console.ReadKey();
+            }
+            catch (Exception falha)
+            {
+                Console.WriteLine($">{falha.Message}");
+                Console.ReadKey();
+            }
+        }
+    }
+}
